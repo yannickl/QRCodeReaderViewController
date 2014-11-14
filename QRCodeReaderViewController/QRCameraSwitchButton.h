193a8dbf6adab4500dd3d14c0d7e9cf64ba9b43d
Yannick Loriot <yannick.loriot@gmail.com>
2014-11-14 23:39:19 +0100

    [UPDATE] Documentation

--- QRCodeReaderViewController/QRCameraSwitchButton.h
+++ QRCodeReaderViewController/QRCameraSwitchButton.h
@@ -26,10 +26,41 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ * The camera switch button.
+ * @since 2.0.0
+ */
 @interface QRCameraSwitchButton : UIButton
+
+#pragma mark - Managing Properties
+/** @name Managing Properties */
+
+/**
+ * @abstract The edge color of the drawing.
+ * @discussion The default color is the white.
+ * @since 2.0.0
+ */
 @property (nonatomic, strong) UIColor *edgeColor;
+
+/**
+ * @abstract The fill color of the drawing.
+ * @discussion The default color is the darkgray.
+ * @since 2.0.0
+ */
 @property (nonatomic, strong) UIColor *fillColor;
+
+/**
+ * @abstract The edge color of the drawing when the button is touched.
+ * @discussion The default color is the white.
+ * @since 2.0.0
+ */
 @property (nonatomic, strong) UIColor *edgeHighlightedColor;
+
+/**
+ * @abstract The fill color of the drawing when the button is touched.
+ * @discussion The default color is the black.
+ * @since 2.0.0
+ */
 @property (nonatomic, strong) UIColor *fillHighlightedColor;
 
 @end
--- QRCodeReaderViewController/QRCodeReaderView.h
+++ QRCodeReaderViewController/QRCodeReaderView.h
@@ -28,6 +28,7 @@
 
 /**
  * Simple view to display an overlay (a square) over the camera view.
+ * @since 2.0.0
  */
 @interface QRCodeReaderView : UIView
 
